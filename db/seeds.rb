31eb407b70a0c63c094fd464ad30fd6673bc575b
madelinemc <madeline.coons@gmail.com>
2021-01-26 19:51:19 -0600

    added seed file for Flavor and ToppingType

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Flavor.destroy_all
+ToppingType.destroy_all
+
+choc = Flavor.create(name: "Chocolate")
+van = Flavor.create(name: "Vanilla")
+straw = Flavor.create(name: "Strawberry")
+mint = Flavor.create(name: "Mint Chip")
+cookie = Flavor.create(name: "Cookie Dough")
+pecan = Flavor.create(name: "Butter Pecan")
+
+carm = ToppingType.create(name: "Caramel Sauce")
+strawb = ToppingType.create(name: "Strawberry Sauce")
+fudge = ToppingType.create(name: "Fudge Sauce")
+whip = ToppingType.create(name: "Whip Cream")
+sprink = ToppingType.create(name: "Sprinkles")
+crunch = ToppingType.create(name: "Toffee Crunchies")
+cherry = ToppingType.create(name: "Cherry on Top")
